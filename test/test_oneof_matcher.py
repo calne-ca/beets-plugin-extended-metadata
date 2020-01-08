bc4034bc3c7000ef9d3f0b8153b7875b2b79b2f5
calne-ca <joschad13@live.de>
2020-01-08 22:50:24 +0100

    Refactoring

--- test/test_oneof_matcher.py
+++ test/test_oneof_matcher.py
@@ -10,84 +10,66 @@ class ExtendedMetaDataMatchQueryTest(unittest.TestCase):
         self.extended_metadata = 'EMD: eyJvcmlnaW4iOiJrb3JlYSIsImxhbmd1YWdlIjpbImtvcmVhbiIsImVuZ2xpc2giXSwidm9jYWxfZ2VuZGVyIjoibWFsZSIsICJnZW5yZSI6ImstcG9wIiwgInRhZyI6WyJ0d28gd29yZHMiXSwgInRlc3QiOiJ0d28gd29yZHMiLCAianBuX3dvcmQiOiLjgZHjgYTjgYrjgpMhIn0='
         self.sut = ExtendedMetaDataMatchQuery(None, None)
 
-    def test_value_match_invalid_pattern(self):
+    # INVALID PATTERNS #
+    def test_invalid_pattern(self):
         self.assertFalse(self.sut.value_match('language:,', self.extended_metadata))
         self.assertFalse(self.sut.value_match('language:,korean', self.extended_metadata))
         self.assertFalse(self.sut.value_match('language:korean,', self.extended_metadata))
         self.assertFalse(self.sut.value_match('language:korean,english,', self.extended_metadata))
         self.assertFalse(self.sut.value_match('language:!', self.extended_metadata))
 
-    def test_value_match_empty_json(self):
+    def test_empty_json(self):
         self.assertFalse(self.sut.value_match('origin:', '{}'))
 
-    def test_value_match_empty_non_json(self):
+    def test_empty_non_json(self):
         self.assertFalse(self.sut.value_match('origin:', 'korea'))
 
-    def test_value_match_empty_pattern(self):
+    def test_empty_pattern(self):
         self.assertTrue(self.sut.value_match('origin:', self.extended_metadata))
 
-    def test_value_match_existing_pattern(self):
+    # SINGLE VALUE #
+    def test_valid_pattern(self):
         self.assertTrue(self.sut.value_match('origin:korea', self.extended_metadata))
         self.assertTrue(self.sut.value_match('genre:k-pop', self.extended_metadata))
 
-    def test_value_match_existing_pattern_different_case(self):
+    def test_different_case(self):
         self.assertTrue(self.sut.value_match('origin:Korea', self.extended_metadata))
 
-    def test_value_match_existing_pattern_unicode_characters(self):
+    def test_unicode_characters(self):
         self.assertTrue(self.sut.value_match('jpn_word:けいおん!', self.extended_metadata))
 
-    def test_value_match_negated_pattern(self):
+    def test_negated(self):
         self.assertFalse(self.sut.value_match('origin:!korea', self.extended_metadata))
         self.assertTrue(self.sut.value_match('origin:!usa', self.extended_metadata))
         self.assertTrue(self.sut.value_match('tag:!vocaloid', self.extended_metadata))
 
-    def test_value_match_space_in_pattern(self):
+    def test_spaces(self):
         self.assertTrue(self.sut.value_match('tag:two words', self.extended_metadata))
         self.assertTrue(self.sut.value_match('test:two words', self.extended_metadata))
 
-    def test_value_match_array_negated_pattern(self):
+    # MULTI VALUE #
+    def test_array_negated(self):
         self.assertFalse(self.sut.value_match('language:!korean', self.extended_metadata))
         self.assertTrue(self.sut.value_match('language:!japanese', self.extended_metadata))
         self.assertTrue(self.sut.value_match('language:!korean,english', self.extended_metadata))
         self.assertFalse(self.sut.value_match('language:!korean,!english,', self.extended_metadata))
 
-    def test_value_match_array_multiple_existing_pattern(self):
+    def test_array_one_of_match(self):
         self.assertTrue(self.sut.value_match('language:korean,japanese', self.extended_metadata))
         self.assertTrue(self.sut.value_match('language:chinese,english', self.extended_metadata))
         self.assertFalse(self.sut.value_match('language:chinese,japanese', self.extended_metadata))
 
-    def test_value_match_array_empty_pattern(self):
+    def test_array_empty_pattern(self):
         self.assertTrue(self.sut.value_match('language:', self.extended_metadata))
 
-    def test_value_match_non_existing_pattern(self):
+    def test_non_existing_pattern(self):
         self.assertFalse(self.sut.value_match('origin:usa', self.extended_metadata))
 
-    def test_value_match_array_existing_pattern(self):
+    def test_array_existing_pattern(self):
         self.assertTrue(self.sut.value_match('language:korean', self.extended_metadata))
 
-    def test_value_match_array_existing_pattern_different_case(self):
+    def test_array_existing_pattern_different_case(self):
         self.assertTrue(self.sut.value_match('language:Korean', self.extended_metadata))
 
-    def test_value_match_array_non_existing_pattern(self):
+    def test_array_non_existing_pattern(self):
         self.assertFalse(self.sut.value_match('language:japanese', self.extended_metadata))
-
-    def test_value_match_regex_invalid_pattern(self):
-        self.assertFalse(self.sut.value_match('language::^$^', self.extended_metadata))
-
-    def test_value_match_regex_empty_pattern(self):
-        self.assertTrue(self.sut.value_match('origin::', self.extended_metadata))
-
-    def test_value_match_regex_existing_pattern(self):
-        self.assertTrue(self.sut.value_match('origin::^.*ea$', self.extended_metadata))
-
-    def test_value_match_regex_non_existing_pattern(self):
-        self.assertFalse(self.sut.value_match('origin::us.*', self.extended_metadata))
-
-    def test_value_match_array_regex_empty_pattern(self):
-        self.assertTrue(self.sut.value_match('language::', self.extended_metadata))
-
-    def test_value_match_array_regex_existing_pattern(self):
-        self.assertTrue(self.sut.value_match('language::^.*an$', self.extended_metadata))
-
-    def test_value_match_array_regex_non_existing_pattern(self):
-        self.assertFalse(self.sut.value_match('language::ja.*', self.extended_metadata))
